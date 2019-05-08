d6746221bbfe6ff1e4ebf191ed36005e49b6b11e
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-05-08 16:17:05 +0100

    Added command logic
    Tweaked other commands

--- src/main/java/UthalBot/command/HelpCommand.java
+++ src/main/java/UthalBot/command/HelpCommand.java
@@ -1,11 +1,26 @@
 package UthalBot.command;
 
+import UthalBot.UthalBot;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageDecoration;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
-public class HelpCommand implements MessageCreateListener {
+import java.util.EnumSet;
+
+public class HelpCommand extends BotCommands implements MessageCreateListener {
     @Override
     public void onMessageCreate(MessageCreateEvent event) {
-        //
+        this.prefix = UthalBot.prefix;
+        if(event.getMessageAuthor().isBotUser()){
+            return;
+        }
+        if(event.getMessageContent().equalsIgnoreCase(prefix+CMD.HELP)){
+            MessageBuilder helpMsg = new MessageBuilder()
+                    .append("Command List\n", MessageDecoration.BOLD);
+            EnumSet.allOf(CMD.class)
+                    .forEach(cmd -> helpMsg.append(prefix+cmd.toString().toLowerCase()+"\n"));
+            helpMsg.send(event.getChannel());
+        }
     }
 }
