7041c47b775edc4d17f41a90a3ce35768535e18c
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-07-29 14:33:50 +0100

    Created class for command

---
+++ src/main/java/UthalBot/command/ValCommand.java
@@ -0,0 +1,13 @@
+package UthalBot.command;
+
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.listener.message.MessageCreateListener;
+
+public class ValCommand extends BotCommands implements MessageCreateListener {
+
+    @Override
+    public void onMessageCreate(MessageCreateEvent event){
+        //
+    }
+
+}
