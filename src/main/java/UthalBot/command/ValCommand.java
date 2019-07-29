370c2a7e1fd675ddfa050756f9b7df706acf3f4e
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-07-29 14:37:06 +0100

    Added functionality to command

--- src/main/java/UthalBot/command/ValCommand.java
+++ src/main/java/UthalBot/command/ValCommand.java
@@ -1,5 +1,8 @@
 package UthalBot.command;
 
+import UthalBot.UthalBot;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageDecoration;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.javacord.api.listener.message.MessageCreateListener;
 
@@ -7,7 +10,15 @@ public class ValCommand extends BotCommands implements MessageCreateListener {
 
     @Override
     public void onMessageCreate(MessageCreateEvent event){
-        //
+        this.prefix = UthalBot.prefix;
+        if(event.getMessageAuthor().isBotUser()){
+            return;
+        }
+        if(event.getMessageContent().equalsIgnoreCase(prefix+CMD.VAL)){
+            new MessageBuilder()
+                    .append("\"BASTARD!\"", MessageDecoration.BOLD)
+                    .send(event.getChannel());
+        }
     }
 
 }
