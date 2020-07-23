41b2d66e01badeb4592c9446105047ffded4ac32
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2020-07-23 23:00:34 +0100

    Added config

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const Discord = require('discord.js');
+const {prefix, token} = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
     console.log('UthalBot Ready.');
 });
 
-client.login('login-token')
+client.login(token)
 
 client.on('message', message => {
-    console.log(message.content);
+    if (message.content === `${prefix}ping`) {
+        message.channel.send('Pong.');
+    }
 });
