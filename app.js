b42a483a8377ab5d553e83354cb8e69c9443575e
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2021-02-05 11:17:51 +0000

    Added more logging

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
+const logger = require('pino')({
+    prettyPrint: true
+});
+
+logger.info('Bot Version: v1.2')
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -12,26 +17,33 @@ for (const file of commandFiles) {
 }
 
 client.once('ready', () => {
-    console.log('UthalBot Ready.');
+    logger.info('UthalBot Ready.');
 });
 
-client.login(token)
+client.login(token).catch(function(e) {
+   logger.error(e);
+});
 
 client.on('message', message => {
-    if (/steelstorm/i.test(message.content))
-    {
-        message.react('⚔');
+    if (/steelstorm/i.test(message.content)) {
+        message.react('⚔').catch(function(e) {
+            logger.error(e);
+        });
     }
 
     if (!message.content.startsWith(prefix) || message.author.bot) return;
+    logger.debug('Valid message read.');
     const args = message.content.slice(prefix.length).trim().split(/ +/);
     const commandName = args.shift().toLowerCase();
 
     const command = client.commands.get(commandName);
 
-    if (!command) return;
-    
+    if (!command) {
+        logger.debug('Invalid command.')
+        return;
+    }
     if (command.args && !args.length) {
+        logger.debug('Invalid args received.')
         let reply = `No args provided, ${message.author}.`;
         if (command.usage) {
             reply += `\nThe proper usage would be: \`${prefix}${commandName} ${command.usage}\``;
@@ -40,9 +52,10 @@ client.on('message', message => {
     }
 
     try {
+        logger.info('Executing: '+ message.content)
         command.execute(message, args);
     } catch (error) {
-        console.error(error);
+        logger.error(error);
         message.reply('An error occurred.');
     }
 });
